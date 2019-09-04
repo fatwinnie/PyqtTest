fe19c9db7e4390076e4a6a84fba9d10f36bf7896
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-09-05 01:24:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import time
 import pyqtgraph as py
 import numpy as np
 from PyQt5 import QtGui,QtCore
+from PyQt5.QtCore import Qt,QRect
 
 
 class CamShow(QMainWindow,Ui_CamShow):
@@ -17,11 +18,14 @@ class CamShow(QMainWindow,Ui_CamShow):
         self.setupUi(self)
         self.PrepWidgets()
         self.PrepParameters()
+        self.Timer = QTimer(self)
+        
         self.CallBackFunctions()
-        self.Timer = QTimer()
-        self.Timer.timeout.connect(self.TimerOutFun)
+        
+        #self.Timer.timeout.connect(self.TimerOutFun)
+        
         self.stopbt = 0
-        self.draw.plotItem.showGrid(True, True, 0.7)
+        #self.draw.plotItem.showGrid(True, True, 0.7)
         
 
     #控件初始化
@@ -47,7 +51,9 @@ class CamShow(QMainWindow,Ui_CamShow):
         self.StopBt.clicked.connect(self.StopCamera)
         self.RecordBt.clicked.connect(self.RecordCamera)
         self.ExitBt.clicked.connect(self.ExitApp)
-        self.btnAdd.clicked.connect(self.update)
+        self.Timer.timeout.connect(self.TimerOutFun)
+        #self.btnAdd.clicked.connect(self.update)
+        #self.btnAdd.clicked.connect(self.drawAvg)
         
         
 
@@ -63,9 +69,9 @@ class CamShow(QMainWindow,Ui_CamShow):
         Y=np.sin(np.arange(points)/points*3*np.pi+time.time())
         C=py.hsvColor(time.time()/5%1,alpha=.5)
         pen=py.mkPen(color=C,width=10)
-        self.draw.plot(X,Y,pen=pen,clear=True)
+        #self.draw.plot(X,Y,pen=pen,clear=True)
         #print("update took %.02f ms"%((time.clock()-t1)*1000))
-        QtCore.QTimer.singleShot(1, self.StartCamera) # QUICKLY repeat 1毫秒執行一次
+       # QtCore.QTimer.singleShot(1, self.StartCamera) # QUICKLY repeat 1毫秒執行一次
         
 
     def StopCamera(self):
@@ -83,21 +89,55 @@ class CamShow(QMainWindow,Ui_CamShow):
 
     def TimerOutFun(self):
         success,self.img=self.camera.read()
+        
+       # print(self.Timer.time())
         self.gray = cv2.cvtColor(self.img,cv2.COLOR_RGB2GRAY)
         if success:   
-            #self.Image = self.ColorAdjust(img)
-            
-            self.DispImg()            
+            self.DispImg()
+            self.CopyImg()
+            self.drawAvg()
+          #  print(np.sum(self.img))
+           # print(self.img)             
             #self.Image_num+=1
+            #inputImg = cv2.imread(self.img)
+            """
+            img2RGB = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
+            #初始化numpy arr
+            r_arr = np.zeros((500,1) , np.float16)
+            g_arr = np.zeros((500,1) , np.float16)
+            b_arr = np.zeros((500,1) , np.float16)
+
+            for i in range(500):
+                r_arr[i,0] = np.sum(img2RGB[10:20 , i , 0]) / 10
+                g_arr[i,0] = np.sum(img2RGB[10:20 , i , 1]) / 10
+                b_arr[i,0] = np.sum(img2RGB[10:20 , i , 2]) /10
+            
+            x = np.linspace(0,500,500)       
+            self.draw_2.plot(x, r_arr[:,0] , pen='r')
+            self.draw_2.plot(x, g_arr[:,0] , pen='g')
+            self.draw_2.plot(x, b_arr[:,0] , pen='b')
+         
+"""
             if self.RecordFlag:
                 self.video_writer.write(self.img)
+
+    
                        
     
     def DispImg(self):
-        CVimg = cv2.cvtColor(self.gray,cv2.COLOR_BGR2RGB)
+        CVimg = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
         qimg = qimage2ndarray.array2qimage(CVimg)
         self.DispLb.setPixmap(QPixmap(qimg))
         self.DispLb.show()
+
+    def CopyImg(self):
+        ret=QRect(50,200,411,10)       
+        CVimg = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)       
+        qimg = qimage2ndarray.array2qimage(CVimg)
+        b=qimg.copy(ret)
+
+        self.DispCopyImg.setPixmap(QPixmap(b))
+        self.DispCopyImg.show()
 
     
     def SetFilePath(self):
@@ -131,6 +171,7 @@ class CamShow(QMainWindow,Ui_CamShow):
             self.StopBt.setEnabled(True)
             self.ExitBt.setEnabled(True)
 
+    """
     def update(self):
         t1=time.clock()
         points=100
@@ -140,9 +181,33 @@ class CamShow(QMainWindow,Ui_CamShow):
         pen=py.mkPen(color=C,width=10)
         self.draw.plot(X,Y,pen=pen,clear=True)
         #print("update took %.02f ms"%((time.clock()-t1)*1000))
-        QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
+        QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat """
 
+    def drawAvg(self):
+        self.draw_2.clear()
+        #inputImg= cv2.imread(self.img)
+        #img = cv2.imread('rgb.png')
+        img2RGB = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
+        #初始化numpy arr
+        r_arr = np.zeros((640,1) , np.uint8)
+        g_arr = np.zeros((640,1) , np.uint8)
+        b_arr = np.zeros((640,1) , np.uint8)
+        for i in range(640):
+            #print(np.sum(self.img[10:20 , i , 0] ))
+            r_arr[i,0] = np.sum(img2RGB[100:110 , i , 0] )/ 10
+            g_arr[i,0] = np.sum(img2RGB[100:110 , i , 1] )/ 10
+            b_arr[i,0] = np.sum(img2RGB[100:110 , i , 2] )/ 10
+        #rint(r_arr[0,0])
 
+        x = np.linspace(0,640,640)
+        self.draw_2.setRange(xRange=[0,640]) # 固定x軸 不會拉動
+        self.draw_2.setRange(yRange=[0,255]) # 固定y軸 不會拉動
+   
+        self.draw_2.plot(x, r_arr[:,0] , pen='r')
+        self.draw_2.plot(x, g_arr[:,0] , pen='g')
+        self.draw_2.plot(x, b_arr[:,0] , pen='b')
+        
+       
 
     def ExitApp(self):
         self.Timer.Stop()
@@ -155,5 +220,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ui=CamShow()
     ui.show()
-    sys.exit(app.exec_())
+    app.exec_()
     print(self.StopBt.setText())
